17f6f79c8659288d04a2b29dda783e4062a753a9
thet1m <timothee.dumas.pro@gmail.com>
2025-05-12 13:40:15 +0200

    Added sound effect (in progress)

---
+++ E4-IG5-Extended-Reality/Assets/Audio/FootstepPlayer.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class FootstepPlayer : MonoBehaviour
+{
+    public AudioClip footstepGrass;
+    public AudioClip footstepWood;
+    public AudioSource audioSource;
+    public LayerMask grassLayer;
+    public LayerMask woodLayer;
+    public float stepInterval = 0.5f;
+
+    private float stepTimer = 0f;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponentInParent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded && controller.velocity.magnitude > 0.1f)
+        {
+            stepTimer += Time.deltaTime;
+            if (stepTimer > stepInterval)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, Vector3.down, out hit, 2f))
+                {
+                    if (((1 << hit.collider.gameObject.layer) & grassLayer) != 0)
+                        audioSource.PlayOneShot(footstepGrass);
+                    else if (((1 << hit.collider.gameObject.layer) & woodLayer) != 0)
+                        audioSource.PlayOneShot(footstepWood);
+                }
+                stepTimer = 0f;
+            }
+        }
+    }
+}
